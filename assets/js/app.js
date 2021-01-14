b87eb00e97a7066b32603e73fece8635db5d43f3
jinzi860209 <jinzi860209@gmail.com>
2021-01-13 22:24:44 -0500

    Rename data file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,25 @@
+ // @TODO: YOUR CODE HERE! 
 var svgWidth = 960; 
-var svgHeight = 910; 
+var svgHeight = 500; 
 
 var margin = {
   top: 20,
   right: 40, 
-  bottom: 60,
+  bottom: 80,
   left: 100
 };
 
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
  
-// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
-var svg = d3.select("#scatter")
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3.select(".scatter")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
+  //Append an SVG group
 var chartGroup = svg.append("g")
   .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
@@ -24,7 +27,7 @@ var chartGroup = svg.append("g")
 var chosenXAxis = "healthcare";
 var chosenYAxis = "poverty";
 
-//Function for Updating x-scale Variable
+// function used for updating x-scale var upon click on axis label
 function xScale(acsData, chosenXAxis) {
     var xLinearScale = d3.scaleLinear()
     .domain([d3.min(acsData, d => d[chosenXAxis] * 0.8),
@@ -46,7 +49,7 @@ function yScale(acsData, chosenYAxis) {
 return yLinearScale;
 }
 
-//Function for Updating xAxis 
+// function used for updating xAxis var upon click on axis label 
 function renderAxes(newXScale, xAxis) {
     var bottomAxis = d3.axisBottom(newXScale);
 
@@ -68,7 +71,8 @@ function renderyAxes(newYScale, yAxis) {
     return yAxis
 }
 
-//Function for Updating Circles on xAxis
+// function used for updating circles group with a transition to
+// new circles
 function renderCircles(circlesGroup, newXScale, chosenXAxis) {
 
     circlesGroup.transition()
@@ -88,13 +92,16 @@ function renderyCircles(circlesGroup, newYScale, chosenYAxis) {
     return circlesGroup;
 }
 
-// Function for Updating Tooltip
+
+// // Function for Updating Tooltip
  function updateToolTip(chosenXAxis, circlesGroup) {
-     if (chosenXAxis === "healthcare") {
-         var label = "Healthcare (%)";
+  
+  var label;
+  if (chosenXAxis === "healthcare") {
+   label = "Healthcare (%)";
      }
-     else {
-        var label = "Obesity (%)";
+  else {
+    label = "Obesity (%)";
      }
 
      var toolTip = d3.tip()
@@ -114,12 +121,13 @@ function renderyCircles(circlesGroup, newYScale, chosenYAxis) {
        toolTip.hide(data);
      });
 
-   return circlesGroup;
+  return circlesGroup;
  }
 
 
-// Import Data
-d3.csv("assets/data/Data.csv").then(function(acsData) {
+// Retrieve data from the CSV file and execute everything below
+d3.csv("assets/data/acsData.csv").then(function(acsData, err) {
+  if (err) throw err;
 
     // Step 1: Parse data
     // ==============================
@@ -223,7 +231,7 @@ d3.csv("assets/data/Data.csv").then(function(acsData) {
         .text("Income ($)");
 
 // updateToolTip function above csv import
-//  var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
+  var circlesGroup = updateToolTip(chosenXAxis, circlesGroup);
 
  // x axis labels event listener
  labelsGroup.selectAll("text")
